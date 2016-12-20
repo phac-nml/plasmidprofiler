769cd5cd411b279ba323f8a2c5d15ce1408386cb
Adrian Zetner <adrian.zetner@phac-aspc.gc.ca>
2016-12-20 12:51:53 -0600

    Added names of papers

--- R/data.R
+++ R/data.R
@@ -13,8 +13,18 @@
 #' (\href{http://www.ncbi.nlm.nih.gov/}{PubMed})
 #'
 #' @source Strains graciously provided by the authors of the following papers:
+#' Complete Genome and Plasmid Sequences of Three Canadian Isolates of
+#' Salmonella enterica subsp. enterica Serovar Heidelberg from Human
+#' and Food Sources. 2016 Labbé et al.
 #' PMID: 26769926
+#'
+#' Complete Sequence of Four Multidrug-Resistant MOBQ1 Plasmids Harboring
+#' blaGES-5 Isolated from Escherichia coli and Serratia marcescens
+#' Persisting in a Hospital in Canada. 2015 Boyd et al.
 #' PMID: 25545311
+#'
+#' Colistin-Nonsusceptible Pseudomonas aeruginosa Sequence Type 654 with
+#' blaNDM-1 Arrives in North America. 2016 Mataseje et al.
 #' PMID: 26824951
 #'
 #' @examples
@@ -36,8 +46,18 @@
 #' (\href{http://www.ncbi.nlm.nih.gov/}{PubMed})
 #'
 #' @source Strains graciously provided by the authors of the following papers:
+#' Complete Genome and Plasmid Sequences of Three Canadian Isolates of
+#' Salmonella enterica subsp. enterica Serovar Heidelberg from Human
+#' and Food Sources. 2016 Labbé et al.
 #' PMID: 26769926
+#'
+#' Complete Sequence of Four Multidrug-Resistant MOBQ1 Plasmids Harboring
+#' blaGES-5 Isolated from Escherichia coli and Serratia marcescens
+#' Persisting in a Hospital in Canada. 2015 Boyd et al.
 #' PMID: 25545311
+#'
+#' Colistin-Nonsusceptible Pseudomonas aeruginosa Sequence Type 654 with
+#' blaNDM-1 Arrives in North America. 2016 Mataseje et al.
 #' PMID: 26824951
 #'
 #' @examples
@@ -59,8 +79,18 @@
 #' (\href{http://www.ncbi.nlm.nih.gov/}{PubMed})
 #'
 #' @source Strains graciously provided by the authors of the following papers:
+#' Complete Genome and Plasmid Sequences of Three Canadian Isolates of
+#' Salmonella enterica subsp. enterica Serovar Heidelberg from Human
+#' and Food Sources. 2016 Labbé et al.
 #' PMID: 26769926
+#'
+#' Complete Sequence of Four Multidrug-Resistant MOBQ1 Plasmids Harboring
+#' blaGES-5 Isolated from Escherichia coli and Serratia marcescens
+#' Persisting in a Hospital in Canada. 2015 Boyd et al.
 #' PMID: 25545311
+#'
+#' Colistin-Nonsusceptible Pseudomonas aeruginosa Sequence Type 654 with
+#' blaNDM-1 Arrives in North America. 2016 Mataseje et al.
 #' PMID: 26824951
 #'
 #' @examples
